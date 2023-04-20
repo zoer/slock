9479c0cac1672d4517ca5e8498556dc746eaf555
Oleg Yashchuk <oazoer@gmail.com>
2023-04-20 19:14:28 +0600

    Rubocop fixes

--- spec/slock/semaphore/health_spec.rb
+++ spec/slock/semaphore/health_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Slock::Semaphore::Health do
   let(:lock_timeout) { 0.4 }
   let(:semaphore) { Slock::Semaphore.new(key, opts) }
   let(:tokens_path) { semaphore.tokens_path }
-  let(:health) { Slock::Semaphore::Health.new(semaphore) }
+  let(:health) { described_class.new(semaphore) }
   let(:token) { rand(0..10).to_s }
 
   let(:size) { 3 }
@@ -17,9 +17,9 @@ RSpec.describe Slock::Semaphore::Health do
     context 'when tokens pool is not changed during the fix' do
       before do
         semaphore # initialize semaphore
-        1.times { missing << redis.lpop(tokens_path) }
+        missing << redis.lpop(tokens_path)
         expect(missing.count).to eq 1
-        allow_any_instance_of(Slock::Semaphore::Lock).to receive(:live?) { false }
+        allow_any_instance_of(Slock::Semaphore::Lock).to receive(:live?).and_return(false)
       end
 
       it 'missing keys must be restored' do
@@ -30,16 +30,20 @@ RSpec.describe Slock::Semaphore::Health do
 
     context 'when tokens pool is changed during the fix' do
       let(:tokens) { size.times.map(&:to_s) }
+
       before do
         semaphore # initialize semaphore
-        1.times { missing << redis.lpop(tokens_path) }
+        missing << redis.lpop(tokens_path)
         expect(missing.count).to eq 1
       end
 
       it 'transaction must be interupted' do
         # add a hook to change tokens pool during Redis transaction
         allow_any_instance_of(Slock::Semaphore::Lock).to \
-          receive(:live?) { redis.lpush(tokens_path, size); false }
+          receive(:live?) {
+            redis.lpush(tokens_path, size)
+            false
+          }
 
         # expecting that missing key is not restored because of cancelled transaction
         expect { health.check }.to change { redis.lrange(tokens_path, 0, -1).sort }
